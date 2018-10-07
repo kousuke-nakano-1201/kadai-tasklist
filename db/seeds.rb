42f9f41880bf21d6988ebf27041a434b8a01412e
kousuke.nakano <ikikewawimps1201@ezweb.ne.jp>
2018-10-07 08:12:58 +0000

    bootstrap design

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create(content: 'test task 1', status: 'test status 1')
+Task.create(content: 'test task 2', status: 'test status 2')
+Task.create(content: 'test task 3', status: 'test status 3')
+
+#(1..100).each do |number|
+#  Task.create(content: 'test task ' + number.to_s, status: 'test status ' + number.to_s)
+#end

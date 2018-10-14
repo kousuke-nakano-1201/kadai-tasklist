6b0137f54151f45f9bb0112513af41e68e3b5131
kousuke.nakano <ikikewawimps1201@ezweb.ne.jp>
2018-10-14 08:55:44 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,13 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in, only:[:show, :new, :create, :edit, :update, :destroy]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
-    @tasks = Task.all.page(params[:page])
+    if logged_in?
+      @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
+#      @tasks = Task.all.page(params[:page])
+    end
   end
 
   def show
@@ -10,11 +15,13 @@ class TasksController < ApplicationController
   end
 
   def new
-    @task = Task.new
+    @task = current_user.tasks.build
+#    @task = Task.new
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
+#    @task = Task.new(task_params)
     
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
@@ -57,5 +64,13 @@ class TasksController < ApplicationController
   
   def task_params
     params.require(:task).permit(:content, :status)
+#    params.require(:task).permit(:content, :status)
+  end
+
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
